dbf22e29ac79bbb8913b2b2ed028abe64d398be9
Happy Enchill <henchill@mit.edu>
2014-06-18 12:53:48 -0400

    adding checks to channels controller

--- src/app/channels/channels.js
+++ src/app/channels/channels.js
@@ -18,12 +18,14 @@ angular.module('Cimba.channels',['ui.router'])
 .controller('ChannelsCtrl', function ChannelsController($scope, $http, $location, $sce){
     $scope.channelKeys = {};
     if ($scope.$parent.userProfile.storagespace !== undefined) {
+        $scope.$parent.loading = true;
         var storage = $scope.$parent.userProfile.storagespace;
         var webid = $scope.$parent.userProfile.webid;
 
         $scope.channelKeys = $scope.$parent.getChannels(storage, webid, true, false);
 
+    } else {
+        $scope.$parent.gotstorage = false;
     }
-    console.log("printing channel keys");
-    console.log($scope.channelKeys);
+    $scope.$parent.loading = false;
 });
