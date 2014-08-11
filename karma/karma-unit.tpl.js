1a2b5904b22d25d3149425308c404e1fa666ebf7
deiu <andrei@fcns.eu>
2014-08-11 19:30:11 -0400

    Version bump. Removed Chrome as test browser for now.

--- karma/karma-unit.tpl.js
+++ karma/karma-unit.tpl.js
@@ -18,7 +18,7 @@ module.exports = function ( karma ) {
       'src/assets/**/*.js'
     ],
     frameworks: [ 'jasmine' ],
-    plugins: [ 'karma-jasmine', 'karma-firefox-launcher', 'karma-coffee-preprocessor','karma-chrome-launcher' ],
+    plugins: [ 'karma-jasmine', 'karma-firefox-launcher', 'karma-coffee-preprocessor' ],
     preprocessors: {
       '**/*.coffee': 'coffee',
     },
@@ -55,8 +55,7 @@ module.exports = function ( karma ) {
      * the aesthetic advantage of not launching a browser every time you save.
      */
     browsers: [
-      'Firefox',
-      'Chrome'
+      'Firefox'
     ]
   });
 };
